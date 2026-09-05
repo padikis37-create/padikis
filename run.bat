676e2ceeb0561ab83868e8416a84f2f3dbb9d412
padikis37-create <padikis37@gmail.com>
2026-09-05 03:26:54 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,26 +1,13 @@
 @echo off
 chcp 65001 > nul
-title Installer & Launcher
+echo Установка необходимых модулей Python...
+echo.
 
-:: Переход в рабочую директорию батника
-cd /d "%~dp0"
-
-if not exist "main.py" (
-    echo [!] Ошибка: Файл main.py не найден в текущей папке.
-    pause
-    exit
-)
-
-echo Установка библиотек selenium и webdriver-manager...
+python -m pip install --upgrade pip
 python -m pip install selenium webdriver-manager
 
 echo.
-echo Запуск main.py...
-echo --------------------------------------------------
-
-python main.py
-if %errorlevel% neq 0 (
-    py -3 main.py
-)
-
+echo ====================================
+echo Установка завершена!
+echo ====================================
 pause
